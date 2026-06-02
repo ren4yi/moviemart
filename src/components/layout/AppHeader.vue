1b53209d0b403a580f09e3e15943f6425a8be67d
lucaspoliveira3 <lucaspoliveira220@gmail.com>
2026-06-02 14:34:25 -0300

    FEAT - Criação da base do header

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template> 
+<header>
+    <nav>
+        <ul>
+            <li>
+                <RouterLINK to="/">
+                    <img src="" alt="Logo do MovieMart">
+                </RouterLINK>
+            </li>
+            <li>
+                <RouterLINK to="/">Movies</RouterLINK>
+            </li>
+            <li>
+                <RouterLINK to="/">Series</RouterLINK>
+            </li>
+            <li>
+                <RouterLINK to="/">New Releases</RouterLINK>
+            </li>
+            <li>
+                <RouterLINK to="/">Trending</RouterLINK>
+            </li>
+            <li>
+                <RouterLINK to="/">Favorites</RouterLINK>
+            </li>
+            <li>
+                <RouterLINK to="/">Team</RouterLINK>
+            </li>
+            <li>
+                <RouterLINK to="/">Cart</RouterLINK>
+            </li>
+            <li>
+                <RouterLINK to="/">Perfil</RouterLINK>
+            </li>
+        </ul>
+    </nav>
+</header>
+</template>
+
+<style scoped>
+
+</style>
